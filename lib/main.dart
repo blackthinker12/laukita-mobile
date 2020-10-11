fb950444563e9f6711f64516e83354523923d204
hermawan <rizal.hermawan@locatorlogic.com>
2020-10-11 11:57:23 +0700

    setting firebase

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   OneSignal.shared.init(
-    "be52446d-33e6-4c66-a641-95e6ff5af719",
+    onesignalKey,
     iOSSettings: null
   );
   OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
